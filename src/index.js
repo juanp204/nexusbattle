a57a34886aeacdaa7bd08edcc8859426919da3c4
Ubuntu <tnb2@TheNexusBattle2.raffnrbe0pquvixhsufzzpftpa.cx.internal.cloudapp.net>
2023-09-26 20:17:00 -0500

    Mensaje descriptivo de los cambios realizados

---
+++ src/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+//dotenv var.entorno
+//const dotenv = require("dotenv");
+//dotenv.config({path:path.join(__dirname,'/env/.env')})
+
+//configuracion
+app.set('port', 80);
+//app.engine('html', require('ejs').renderFile);
+//app.set('view engine', 'ejs');
+
+//var.session
+const session = require('express-session')
+app.use(session({
+    secret:'secret',
+    resave: true,
+    saveUninitialized:true
+}));
+
+//DB
+const conectado = require('./database/db');
+const req = require('express/lib/request');
+
+//rutas
+app.use(require('./routes/index.js'));
+
+//recursos
+app.use(express.static('public'));
+
+//server
+app.listen(app.get('port'),()=>{
+    console.log("server on :"+app.get('port'));
+});
+
+
