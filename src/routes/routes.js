7217b73bad6fd49c67cd5ef61c01b17f0e604865
juanp204 <juanp.adames2004@gmail.com>
2023-10-09 17:13:31 -0500

    problem solucionated routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -55,7 +55,7 @@ console.log(route);
 
 router.get('/', async (req, res) => {
     //res.render(path.join(route,'views/index.html'));
-    res.sendFile('./20.109.18.73/index.html', { root: rootDir }, (err) => {
+    res.sendFile('../20.109.18.73/index.html', { root: rootDir }, (err) => {
         if (err) {
             // Handle any errors that occur during sending the file
             console.error(err);
@@ -162,7 +162,7 @@ router.get('/redirect', (req, res) => {
             req.session.sessionParams = { user: response.account, idToken: response.idToken };
             console.log("\nAuthToken: \n" + JSON.stringify(response));
             //res.render('signin', { showSignInButton: false, givenName: response.account.idTokenClaims.given_name });
-            res.sendFile('./20.109.18.73/index.html', { root: rootDir }, (err) => {
+            res.sendFile('../20.109.18.73/index.html', { root: rootDir }, (err) => {
                 if (err) {
                     // Handle any errors that occur during sending the file
                     console.error(err);
@@ -181,7 +181,7 @@ router.get('/redirect', (req, res) => {
                 //Send the user home with some message
                 //But always check if your session still exists
                 //res.render('signin', { showSignInButton: false, givenName: req.session.sessionParams.user.idTokenClaims.given_name, message: 'User has cancelled the operation' });
-                res.sendFile('./20.109.18.73/index.html', { root: rootDir }, (err) => {
+                res.sendFile('../20.109.18.73/index.html', { root: rootDir }, (err) => {
                     if (err) {
                         // Handle any errors that occur during sending the file
                         console.error(err);
@@ -204,7 +204,7 @@ router.get('/redirect', (req, res) => {
             req.session.sessionParams = { user: response.account, idToken: response.idToken };
             console.log("\AuthToken: \n" + JSON.stringify(response));
             //res.render('signin', { showSignInButton: false, givenName: response.account.idTokenClaims.given_name });
-            res.sendFile('./20.109.18.73/index.html', { root: rootDir }, (err) => {
+            res.sendFile('../20.109.18.73/index.html', { root: rootDir }, (err) => {
                 if (err) {
                     // Handle any errors that occur during sending the file
                     console.error(err);
