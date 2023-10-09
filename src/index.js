272ab96a5bcee4193c40da5745c17fd2b6617dda
Ubuntu <tnb2@TheNexusBattle2.raffnrbe0pquvixhsufzzpftpa.cx.internal.cloudapp.net>
2023-10-09 15:53:08 -0500

    directorio activo

--- src/index.js
+++ src/index.js
@@ -1,26 +1,41 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const session = require('express-session');
+const { engine } = require('express-handlebars');
+const msal = require('@azure/msal-node');
+const app = express();
 
-app.use(express.urlencoded({extended:false}));
-app.use(express.json());
+//app.use(express.urlencoded({extended:false}));
+//app.use(express.json());
 
 //dotenv var.entorno
-//const dotenv = require("dotenv");
-//dotenv.config({path:path.join(__dirname,'/env/.env')})
+const dotenv = require("dotenv");
+dotenv.config({path:path.join(__dirname,'/env/.env')})
 
 //configuracion
-app.set('port', 8888);
 //app.engine('html', require('ejs').renderFile);
 //app.set('view engine', 'ejs');
 
+
+/**
+ * Using express-session middleware. Be sure to familiarize yourself with available options
+ * and set them as desired. Visit: https://www.npmjs.com/package/express-session
+ */
+
 //var.session
-const session = require('express-session')
-app.use(session({
-    secret:'secret',
-    resave: true,
-    saveUninitialized:true
-}));
+const sessionConfig = {
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        secure: true, // set this to true on production
+    }
+}
+
+app.use(session(sessionConfig));
+
+
+
 
 //DB
 //const conectado = require('./database/mysql.js');
@@ -33,6 +48,6 @@ app.use(express.static('20.109.18.73'));
 app.use(require('./routes/routes.js'));
 
 //server
-app.listen(app.get('port'),()=>{
-    console.log("server on :"+app.get('port'));
+app.listen(process.env.SERVER_PORT,()=>{
+    console.log("server on :"+ process.env.SERVER_PORT);
 });
